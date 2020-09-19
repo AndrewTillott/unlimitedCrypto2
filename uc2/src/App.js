555fa1465f3dd12ab0b5191c48e47357d1d34130
AndrewTillott <andytillott@hotmail.co.uk>
2020-09-19 15:59:13 +0100

    nav bar

---
+++ uc2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Home from "./pages/home/index";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ uc2/src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Navbar from '../../components/navbar/NavBar.jsx'
+
+class Home extends Component {
+    state = {};
+    render(){
+        return(
+            <>
+                <Navbar />
+            </>
+        )
+    }
+}
+
+export default Home;
